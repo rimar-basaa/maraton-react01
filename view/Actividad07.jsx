36d32831e4a6f70705dff2b6f55504a123f05cc3
rimar basaa <rimar.basaa@gmail.com>
2024-03-01 23:22:46 -0300

    agrega btn borrar

--- view/Actividad07.jsx
+++ view/Actividad07.jsx
@@ -26,16 +26,19 @@ const Actividad07 = () => {
         }
       ];
     
+    const borrar = (e) => {
+        e.target.parentNode.remove();
+    };    
 
     const tags = avengers.map(hero =>
         <div className="tag" key={hero.nombre}> 
-        <p>El superheroe <span className='rojo'>{hero.nombre.toUpperCase()}</span> su principal poder es la <span>{hero.poder}</span></p>
-        <img src={"../src/img/kid-" + hero.nombre + "-removebg.png"} alt="" />
+            <p>El superheroe <span className='rojo'>{hero.nombre.toUpperCase()}</span> su principal poder es la <span>{hero.poder}</span></p>
+            <img src={"../src/img/kid-" + hero.nombre + "-removebg.png"} alt="" />
+            <button className='btnErase' onClick={borrar}>Borrar</button>
         </div>
     );
 
     return (
-
         <div className="box-main">
             {tags}            
         </div>
